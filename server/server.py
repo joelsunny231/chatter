e6a5c30f822c373d01a206d8e97215a2ae306d5d
joelsunny231 <joelsunnypro@gmail.com>
2017-08-29 23:14:45 +0530

    Add files via upload

--- server/server.py
+++ server/server.py
@@ -1,1 +1,26 @@
-#
+
+import sqlite3 as sql
+import socket
+import threading
+
+class server():
+	def __init__(self, port):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.host = socket.gethostname()
+		self.port = port 
+		self.sock.bind((host,port))
+		self.sock.listen(5)
+
+def client_thread(client):
+	client.send('ACK')
+	
+def main():
+	myServer = server(1235);
+	db = sql.connect('chat_server.db')
+	db_c = db.cursor()
+	
+	while 1:
+		client, addr = myServer.sock.accept()
+		threading.Thread(target = client_thread, args = (client,))
+		
+main()
