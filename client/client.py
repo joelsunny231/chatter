6cba1ed1768473c3b51a9ebeb9d85653ad0b1316
joelsunny231 <joelsunnypro@gmail.com>
2017-08-29 23:15:29 +0530

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -1,1 +1,14 @@
+import socket
 
+class client():
+	def __init__(self, host, port):
+		self.sock = socket.socket()
+		self.host = host 
+		self.port = port
+		self.sock.connect((host,port))
+
+def main():
+	myClient = client('127.0.0.1', 1235);
+	print myClient.recv()
+
+main()
