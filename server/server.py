d686bd8824710ad9f30136f9042754a510cec657
joelsunny231 <joelsunnypro@gmail.com>
2017-08-31 12:17:31 +0530

    modified client_thread()

--- server/server.py
+++ server/server.py
@@ -6,13 +6,27 @@ import threading
 class server():
 	def __init__(self, port):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.host = socket.gethostname()
+		self.host = '127.0.0.1'
 		self.port = port 
-		self.sock.bind((host,port))
+		self.sock.bind((self.host,self.port))
 		self.sock.listen(5)
 
-def client_thread(client):
-	client.send('ACK')
+class client():
+	def __init__(self, host, port):
+		self.sock = socket.socket()
+		self.host = host 
+		self.port = port
+		self.sock.connect((host,port))
+		
+def client_thread(ct_sock, ct_addr):
+	print "client thread started"
+	ct_sock.send('ACK')
+	
+	# start a p2p connection for sending undelivered data
+	print ct_sock.recv(1024)
+	myClient = client(ct_addr[0], ct_addr[1]) 
+	ct_sock.close()
+	myClient.sock.send('hello')
 	
 def main():
 	myServer = server(1235);
@@ -21,6 +35,8 @@ def main():
 	
 	while 1:
 		client, addr = myServer.sock.accept()
-		threading.Thread(target = client_thread, args = (client,))
+		print "connected to " + str(client)+ " with address " + str(addr)
+		t = threading.Thread(target = client_thread, args = (client, addr,))
+		t.start()
 		
 main()
